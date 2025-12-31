020b3eaa465d6bfc7c3c8b15cb2957b543beff3a
Zurab Mujirishvili <zurab.mujirishvili@fau.de>
2025-12-31 12:34:20 +0100

    Add factories for available risk metrics

--- trajax/costs/risk/factory.py
+++ trajax/costs/risk/factory.py
@@ -12,9 +12,18 @@ class risk:
 
     class numpy:
         @staticmethod
+        def expected_value(*, sample_count: int) -> NumPyRiskMetric:
+            return RisKitRiskMetric.create(
+                backend=rk.backend.numpy.create(),
+                creator=lambda *, cost, backend: rk.risk.expected_value_of(
+                    cost, backend=backend
+                ).sampled_with(rk.sampler.monte_carlo(sample_count)),
+            )
+
+        @staticmethod
         def mean_variance(*, gamma: float, sample_count: int) -> NumPyRiskMetric:
             return RisKitRiskMetric.create(
-                backend=rk.backend.numpy(),
+                backend=rk.backend.numpy.create(),
                 creator=lambda *, cost, backend: rk.risk.mean_variance_of(
                     cost, backend=backend, gamma=gamma
                 ).sampled_with(rk.sampler.monte_carlo(sample_count)),
@@ -22,9 +31,18 @@ class risk:
 
     class jax:
         @staticmethod
+        def expected_value(*, sample_count: int) -> JaxRiskMetric:
+            return RisKitRiskMetric.create(
+                backend=rk.backend.jax.create(),
+                creator=lambda *, cost, backend: rk.risk.expected_value_of(
+                    cost, backend=backend
+                ).sampled_with(rk.sampler.monte_carlo(sample_count)),
+            )
+
+        @staticmethod
         def mean_variance(*, gamma: float, sample_count: int) -> JaxRiskMetric:
             return RisKitRiskMetric.create(
-                backend=rk.backend.jax(),
+                backend=rk.backend.jax.create(),
                 creator=lambda *, cost, backend: rk.risk.mean_variance_of(
                     cost, backend=backend, gamma=gamma
                 ).sampled_with(rk.sampler.monte_carlo(sample_count)),
